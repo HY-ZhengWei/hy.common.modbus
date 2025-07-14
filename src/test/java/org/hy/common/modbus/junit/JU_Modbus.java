71a2944a69c195cddc586806ce6bead9913c8f9a
HY-ZhengWei <HY.ZhengWei@qq.com>
2025-07-14 10:40:27 +0800

    1.0.3
    1. 添加：LED显示屏

--- src/test/java/org/hy/common/modbus/junit/JU_Modbus.java
+++ src/test/java/org/hy/common/modbus/junit/JU_Modbus.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.hy.common.Date;
 import org.hy.common.Help;
+import org.hy.common.StringHelp;
 import org.hy.common.modbus.IModbus;
 import org.hy.common.modbus.data.MConnConfig;
 import org.hy.common.modbus.data.MDataItem;
@@ -151,6 +152,169 @@ public class JU_Modbus
             Help.print(v_Nums);
             Thread.sleep(1000);
         }
+    }
+    
+    
+    
+    @Test
+    public void test_Modbus4j_TCP_LED显示屏_报文批量写()
+    {
+        List<MDataItem> v_Datagram = new ArrayList<MDataItem>();
+        v_Datagram.add(new MDataItem("时间1" ,0    ,ModbusData.DVarchar         ," 10:00:00"));
+        v_Datagram.add(new MDataItem("温度1" ,8    ,ModbusData.DVarchar         ,"1000C"));
+        v_Datagram.add(new MDataItem("时间2" ,16   ,ModbusData.DVarchar         ," 09:00:00"));
+        v_Datagram.add(new MDataItem("温度2" ,4291 ,ModbusData.D2B_Int_Signed   ,"1200"));
+        v_Datagram.add(new MDataItem("时间3" ,32   ,ModbusData.DVarchar         ," 08:00:00"));
+        v_Datagram.add(new MDataItem("温度3" ,4293 ,ModbusData.D2B_Int_Unsigned ,"1500"));
+        v_Datagram.add(new MDataItem("时间4" ,48   ,ModbusData.DVarchar         ," 07:00:00"));
+        v_Datagram.add(new MDataItem("温度4" ,56   ,ModbusData.DVarchar         ,"1600C"));
+        
+        MConnConfig v_Config = new MConnConfig();
+        v_Config.setHost("192.168.0.101");
+        v_Config.setPort(502);
+        v_Config.setProtocol(ModbusProtocol.TCP);
+        v_Config.setType(    ModbusType.Master);
+        
+        IModbus v_Modbus = new Modbus4J(v_Config);
+        v_Modbus.init();
+        
+        v_Modbus.writes(1 ,v_Datagram);
+    }
+    
+    
+    
+    @Test
+    public void test_Modbus4j_TCP_LED显示屏() throws InterruptedException
+    {
+        Thread.sleep(10000);
+        
+        MConnConfig v_Config = new MConnConfig();
+        v_Config.setHost("192.168.0.101");
+        v_Config.setPort(502);
+        v_Config.setProtocol(ModbusProtocol.TCP);
+        v_Config.setType(    ModbusType.Master);
+        
+        IModbus v_Modbus = new Modbus4J(v_Config);
+        v_Modbus.init();
+        
+        List<LEDData> v_Datas  = new ArrayList<LEDData>();
+        int           v_Index  = -1;
+        int           v_TimeGS = 5;
+        
+        for (int x=1000; x<=160000; x++)
+        {
+            Date v_Time = new Date();
+            v_Time = v_Time.getTimeGroupSecond(v_TimeGS);
+            
+            LEDData v_Data = new LEDData();
+            v_Data.setTime(v_Time);
+            v_Data.setTemperature(Help.random(0 ,2000));
+            
+            if ( v_Index >= 0 )
+            {
+                if ( v_Datas.get(v_Index).getTime().equals(v_Time) )
+                {
+                    Thread.sleep(1000);
+                    continue;
+                }
+            }
+            
+            v_Datas.add(v_Data);
+            v_Index++;
+            
+            if ( v_Index - 3 >= 0 )
+            {
+                System.out.println(v_Modbus.writeString(1 ,48 ," " + v_Datas.get(v_Index - 3).getTime().getHMS()));
+                System.out.println(v_Modbus.writeString(1 ,56 ,StringHelp.lpad(v_Datas.get(v_Index - 3).getTemperature() ,4 ," ") + "C"));
+            }
+            else
+            {
+                System.out.println(v_Modbus.writeString(1 ,48 ," "));
+                System.out.println(v_Modbus.writeString(1 ,56 ," "));
+            }
+            
+            if ( v_Index - 2 >= 0 )
+            {
+                System.out.println(v_Modbus.writeString(1 ,32 ," " + v_Datas.get(v_Index - 2).getTime().getHMS()));
+                System.out.println(v_Modbus.writeString(1 ,40 ,StringHelp.lpad(v_Datas.get(v_Index - 2).getTemperature() ,4 ," ") + "C"));
+            }
+            else
+            {
+                System.out.println(v_Modbus.writeString(1 ,32 ," "));
+                System.out.println(v_Modbus.writeString(1 ,40 ," "));
+            }
+            
+            if ( v_Index - 1 >= 0 )
+            {
+                System.out.println(v_Modbus.writeString(1 ,16 ," " + v_Datas.get(v_Index - 1).getTime().getHMS()));
+                System.out.println(v_Modbus.writeString(1 ,24 ,StringHelp.lpad(v_Datas.get(v_Index - 1).getTemperature() ,4 ," ") + "C"));
+            }
+            else
+            {
+                System.out.println(v_Modbus.writeString(1 ,16 ," "));
+                System.out.println(v_Modbus.writeString(1 ,24 ," "));
+            }
+            
+            System.out.println(v_Modbus.writeString(1 ,0  ," " + v_Datas.get(v_Index).getTime().getHMS()));
+            System.out.println(v_Modbus.writeString(1 ,8  ,StringHelp.lpad(v_Datas.get(v_Index).getTemperature() ,4 ," ") + "C"));
+            
+            Thread.sleep(1000);
+        }
+    }
+    
+    
+    
+    
+    
+    class LEDData
+    {
+        /** 时间 */
+        private Date    time;
+        
+        /** 温度 */
+        private Integer temperature;
+
+        
+        
+        /**
+         * 获取：时间
+         */
+        public Date getTime()
+        {
+            return time;
+        }
+
+        
+        /**
+         * 设置：时间
+         * 
+         * @param i_Time 时间
+         */
+        public void setTime(Date i_Time)
+        {
+            this.time = i_Time;
+        }
+
+        
+        /**
+         * 获取：温度
+         */
+        public Integer getTemperature()
+        {
+            return temperature;
+        }
+
+        
+        /**
+         * 设置：温度
+         * 
+         * @param i_Temperature 温度
+         */
+        public void setTemperature(Integer i_Temperature)
+        {
+            this.temperature = i_Temperature;
+        }
+        
     }
     
 }
